b86e40dc1675750f29da27b12bce5f23de13f73e
Andrew <andslobby@gmail.com>
2022-05-06 22:39:39 +0300

    refactor: change typein clic options

--- src/forest_cover/train.py
+++ src/forest_cover/train.py
@@ -1,4 +1,4 @@
-from ctypes import Union
+from typing import Union
 from pathlib import Path
 import pandas as pd
 from joblib import dump
@@ -21,74 +21,76 @@ def create_pipeline(
         max_features: Union[int, str, float],
         min_samples_leaf: Union[int, float],
         random_state: int) -> Pipeline:
+
     pipeline_steps = []
-    if use_scaler:
-        pipeline_steps.append(("scaler", StandardScaler()))
     pipeline_steps.append(
         (
             "classifier",
-            DecisionTreeClassifier(max_depth
-
-                                   ),
+            DecisionTreeClassifier(
+                max_depth=max_depth,
+                max_features=max_features,
+                min_samples_leaf=min_samples_leaf,
+                random_state=random_state)
         )
     )
+
     return Pipeline(steps=pipeline_steps)
 
 
-@ click.command()
-@ click.option(
+@click.command()
+@click.option(
     "-i",
     "--input-path",
     default=Path(DATA_DIR).joinpath(DATA_PATH),
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
-    show_default=True,
+    show_default=True
 )
-@ click.option(
+@click.option(
     "-o",
     "--output-path",
     default=Path(OUTPUT_DIR).joinpath(MODEL_PATH),
     type=click.Path(dir_okay=False, writable=True, path_type=Path),
-    show_default=True,
+    show_default=True
 )
-@ click.option(
+@click.option(
     "--random-state",
     default=42,
     type=int,
-    show_default=True,
+    show_default=True
 )
-@ click.option(
+@click.option(
     "--use-scaler",
     default=True,
     type=bool,
-    show_default=True,
+    show_default=True
 )
-@ click.option(
+@click.option(
     "--max-depth",
-    default=None,
-    type=Union[int, None]
-    show_default=True,
+    default=100,
+    type=int,
+    show_default=True
 )
-@ click.option(
+@click.option(
     "--max-features",
-    default=None,
-    type=Union[int, str, click.FloatRange(
-        0, 1, min_open=True, max_open=True), None],
-    show_default=True,
+    default=1,
+    type=click.FloatRange(
+        0, 1, min_open=True, max_open=False),
+    show_default=True
 )
-@ click.option(
+@click.option(
     "--min-samples-leaf",
     default=1,
-    type=Union[int, click.FloatRange(0, 1, min_open=True, max_open=True)],
-    show_default=True,
+    type=int,
+    show_default=True
 )
 def train(
     input_path: Path,
     output_path: Path,
     random_state: int,
     use_scaler: bool,
-    max_depth: Union[int, None],
-    max_features: Union[int, str, float],
-    min_samples_leaf: Union[int, float]
+    max_depth: int,
+    max_features: float,
+    min_samples_leaf: int
 ) -> None:
     features_train, target_train = get_dataset(input_path)
     pipeline = create_pipeline(
